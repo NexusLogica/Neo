a4dd2803f7cf294088b6dffb93b06d2abbb97b0b
I'm Awesome (so please don't delete me) <picklepairs@gmail.com>
2015-09-08 19:58:33 -0700

    i moved the file

--- src/js/cube-scene.js
+++ src/js/cube-scene.js
@@ -6,18 +6,46 @@ var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var material = new THREE.MeshBasicMaterial( { color: 0xff55ff } );
+var geometry = new THREE.BoxGeometry( 9, 9, 9 );
+//var material = new THREE.MeshPhongMaterial( { color: 0xff55ff } );
+var material = new THREE.MeshLambertMaterial({color: 0xff55ff, transparent: true, opacity: 0.3});
 var cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
-camera.position.z = 5;
+var geometry2 = new THREE.BoxGeometry( 1, 1, 1 );
+var material2 = new THREE.MeshPhongMaterial( { color: 0xff99ff } );
+var cube2 = new THREE.Mesh( geometry2, material2 );
+scene.add( cube2 );
+cube2.position.x=2;
+
+var geometry3 = new THREE.BoxGeometry( 1, 1, 1 );
+var material3 = new THREE.MeshPhongMaterial( { color: 0xffffff } );
+var cube3 = new THREE.Mesh( geometry3, material3 );
+scene.add( cube3 );
+cube3.position.x=-2;
+
+camera.position.z = 15;
+
+addLights();
+
+function addLights() {
+    var dirLight = new THREE.DirectionalLight(0xffffff, 1);
+    dirLight.position.set(100, 100, 50);
+    scene.add(dirLight);
+    var ambLight = new THREE.AmbientLight(0x404040);
+    scene.add(ambLight);
+};
 
 var render = function () {
     requestAnimationFrame( render );
 
     cube.rotation.x += 0.01;
     cube.rotation.y += 0.01;
+    cube2.rotation.x += 0.01;
+    cube2.rotation.y += 0.01;
+    cube3.rotation.x += 0.01;
+    cube3.rotation.y += 0.01;
+
 
     renderer.render(scene, camera);
 };
