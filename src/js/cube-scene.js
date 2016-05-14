2d5dbd826e9ea4f8abb0bdd4c2c0758a3beab49e
I'm Awesome (so please don't delete me) <picklepairs@gmail.com>
2016-05-14 14:59:12 -0700

    i made it so we can click on it and make it different colours

--- src/js/cube-scene.js
+++ src/js/cube-scene.js
@@ -8,26 +8,7 @@ document.body.appendChild( renderer.domElement );
 
 var renderSignal = new signals.Signal();
 
-//var geometry = new THREE.BoxGeometry( 9, 9, 9 );
-////var material = new THREE.MeshPhongMaterial( { color: 0xff55ff } );
-//var material = new THREE.MeshLambertMaterial({color: 0xff55ff, transparent: true, opacity: 0.3});
-//var cube = new THREE.Mesh( geometry, material );
-//scene.add( cube );
-//
-//var geometry2 = new THREE.BoxGeometry( 1, 1, 1 );
-//var material2 = new THREE.MeshPhongMaterial( { color: 0xff99ff } );
-//var cube2 = new THREE.Mesh( geometry2, material2 );
-//scene.add( cube2 );
-//cube2.position.x=2;
-//
-//var geometry3 = new THREE.BoxGeometry( 1, 1, 1 );
-//var material3 = new THREE.MeshPhongMaterial( { color: 0xffffff } );
-//var cube3 = new THREE.Mesh( geometry3, material3 );
-//scene.add( cube3 );
-//cube3.position.x=-2;
-
-console.log(1);
-console.log(2);
+var cubeList=[];
 var x=0.0;
 
 for(var i=1; i<10; i++){
@@ -60,6 +41,7 @@ for(var i=1; i<10; i++){
     cube.position.x=x-9;
     cube.position.y=0;
     cube.name=name;
+    cubeList.push(cube);
     scene.add( cube );
 }
 
@@ -100,22 +82,30 @@ canvas3d.on('click', function(event) {
     }
 });
 
+var randomizeColors = function(){
+    for (var i=0;i<cubeList.length;i++){
+        var c=cubeList[i];
+        c.material.color.setRGB(1,1,1);
+    }
+}
+
 var count=0;
 setInterval(function(){
     console.log("count="+count);
     count++;
-},5000);
+    randomizeColors();
+},3000);
 
 var render = function () {
     requestAnimationFrame( render );
 
     renderSignal.dispatch();
 
-    //for(var j=1; j<10; j++)  {
-    //    var cube = scene.getObjectByName("cube"+j, true );
-    //    cube.rotation.x += 0.01;
-    //    cube.rotation.y += 0.01;
-    //}
+    for(var j=1; j<10; j++)  {
+        var cube = scene.getObjectByName("cube"+j, true );
+        cube.rotation.x += 0.01;
+        cube.rotation.y += 0.01;
+    }
     renderer.render(scene, camera);
 };
 
